4856968a698dc991418b7301982791fbd9b9d546
rioaldie <rioaldierwanto@gmail.com>
2024-01-20 15:25:38 +0700

    slicing ui for register page

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,31 +1,57 @@
 const RegisterPage = () => {
   return (
     <div className="w-full min-h-screen flex justify-center items-center">
-      <div className="w-96 h-96 ">
-        <form className="flex flex-col gap-3 justify-center items-center">
-          <p className="text-3xl font-semibold">Login</p>
-          <label htmlFor="email" className="text-slate-500 w-full">
-            Email
-          </label>
+      <div className="w-[600px] h-96 ">
+        <p className="text-3xl font-semibold w-full text-center">
+          Daftar Akun <span className="text-pink-600">Baru</span>
+        </p>
+        <form className="flex w-full mt-5 justify-center flex-wrap flex-row gap-3 flex-grow">
           <input
             type="email"
-            className="block w-full border border-pink-600  text-slate-500 rounded-md h-11 p-4 placeholder-slate-500"
-            placeholder="Masukan Email"
+            className="block w-2/5 border border-gray-300 focus:ring-pink-500 focus:border-pink-500  text-slate-500 rounded-md h-11 p-4 placeholder-slate-500"
+            placeholder="Email"
             autoFocus
             name="email"
           />
-          <label htmlFor="password" className="text-slate-500 w-full">
-            Password
-          </label>
+          <input
+            type="text"
+            className="block w-2/5 border border-gray-300 focus:ring-pink-500 focus:border-pink-500 text-slate-500 rounded-md h-11 p-4 placeholder-slate-500"
+            placeholder="Nama"
+            autoFocus
+            name="name"
+          />
           <input
             type="password"
-            className="block w-full border border-pink-600 text-slate-500 rounded-md h-11 p-4 placeholder-slate-500"
-            placeholder="Masukan Password"
+            className="block w-2/5 border border-gray-300 focus:ring-pink-500 focus:border-pink-500  text-slate-500 rounded-md h-11 p-4 placeholder-slate-500"
+            placeholder="Password"
             autoFocus
             name="password"
           />
+          <input
+            type="password"
+            className="block w-2/5 border border-gray-300 text-slate-500 rounded-md h-11 focus:ring-pink-500 focus:border-pink-500 p-4 placeholder-slate-500"
+            placeholder="Konfirmasi Password"
+            autoFocus
+            name="confirm password"
+          />
+          <div className="w-2/5 h-11">
+            <select
+              id="countries"
+              className="bg-gray-50 border h-11 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500  w-full p-2.5 ">
+              <option value="female">Wanita</option>
+              <option value="male">Pria</option>
+            </select>
+          </div>
+
+          <input
+            type="number"
+            className="block w-2/5 border border-gray-300 text-slate-500 rounded-md h-11 p-4 placeholder-slate-500"
+            placeholder="Umur"
+            autoFocus
+            name="age"
+          />
           <button className="bg-pink-600 rounded mt-3 h-10 w-48 text-white">
-            Masuk
+            Submit
           </button>
         </form>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-row items-center justify-between p-24">
+    <main className="flex min-h-screen flex-row items-center justify-between px-40">
       <div className="w-[500px] flex flex-col justify-start gap-5">
         <h1 className="font-bold text-5xl text-black text-poppins">
           Temukan Minat dan Bakat, Untuk{' '}
@@ -10,7 +10,7 @@ export default function Home() {
         </h1>
         <p className="font-normal text-base">
           Tes minat bakat untuk mendapatkan minat bakat yang tepat
-          bagi mereka, agar potensi bakat mereka dapat dikembangkan
+          bagi anak, agar potensi bakat mereka dapat dikembangkan
           dengan tepat.
         </p>
         <button className="w-40 h-10 bg-pink-600 text-white rounded">
