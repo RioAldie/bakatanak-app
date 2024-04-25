0a1f85bb8007a40a64a6080f22ba010a5f3cdcaa
rioaldie <rioaldierwanto@gmail.com>
2024-04-25 23:55:27 +0700

    deactive cache in course-page

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,6 +1,8 @@
 import CourseDisplay from '@/components/resultItem/courseDisplay';
+import { revalidatePath } from 'next/cache';
 
 const CoursePage = () => {
+  revalidatePath('/course');
   return (
     <div className="w-full min-h-screen flex items-center flex-col ">
       <p className="font-bold text-black mt-20 text-center text-2xl w-[400px]">
