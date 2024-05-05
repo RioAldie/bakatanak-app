b6efe499e6969799f84df7bc098973241e6f2b98
rioaldie <rioaldierwanto@gmail.com>
2024-05-05 22:22:01 +0700

    hide mongo url

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,10 +9,7 @@ export const connectToDb = async () => {
       return;
     }
 
-    const db = await mongoose.connect(
-      process.env.MONGO ||
-        'mongodb+srv://rioaldierwanto:7615006rio@cluster0.4qxzuee.mongodb.net/db_talentexpert'
-    );
+    const db = await mongoose.connect(process.env.MONGO_URL);
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     console.log(error);
