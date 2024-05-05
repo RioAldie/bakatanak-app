787603e3d5c342616d4c131ed087fb5874c618c8
rioaldie <rioaldierwanto@gmail.com>
2024-05-05 22:15:54 +0700

    delete middleware

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { authConfig } from '@/app/authconfig';
-import NextAuth from 'next-auth';
-
-export default NextAuth(authConfig).auth;
-
-export const config = {
-  matcher: ['/((?!api|static|.*\\..*|_next).*)'],
-};
